23368e8dd2b53e325fe67c5e36d0eea6d1743fb6
Zohaib Ahmed <zohaib92@gmail.com>
2018-02-25 14:53:00 -0800

    client should be instantiable

--- lib/configuration.rb
+++ lib/configuration.rb
@@ -4,9 +4,9 @@ module KrakenClient
     attr_accessor :api_key, :api_secret, :base_uri, :limiter_interface,
                   :api_version, :tier, :limiter, :limit_current_count
 
-    def initialize
-      @api_key             = ENV['KRAKEN_API_KEY']
-      @api_secret          = ENV['KRAKEN_API_SECRET']
+    def initialize(api_key, api_secret)
+      @api_key             = api_key
+      @api_secret          = api_secret
       @base_uri            = 'https://api.kraken.com'
       @api_version         = 0
       @limiter             = true
